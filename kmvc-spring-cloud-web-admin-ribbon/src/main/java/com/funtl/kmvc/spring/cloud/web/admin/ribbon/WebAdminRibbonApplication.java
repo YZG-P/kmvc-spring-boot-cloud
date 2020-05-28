b12c926269b71900dbbf8587bbe66da1dd680430
YZG-P <2473589192@qq.com>
2020-05-28 13:08:09 +0800

    初始化项目

---
+++ kmvc-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/kmvc/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -0,0 +1,17 @@
+package com.funtl.kmvc.spring.cloud.web.admin.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix  //熔断
+@EnableHystrixDashboard //仪表盘
+public class WebAdminRibbonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminRibbonApplication.class,args);
+    }
+}
---
+++ kmvc-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/kmvc/spring/cloud/web/admin/ribbon/config/HystrixDashboardConfiguration.java
@@ -0,0 +1,22 @@
+package com.funtl.kmvc.spring.cloud.web.admin.ribbon.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * boot2对比boot1需要增加一个 HystrixMetricsStreamServlet 的配置
+ */
+@Configuration
+public class HystrixDashboardConfiguration {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
